fef2242c294997f57b8aa442270e08898ef31f82
Julijana <julijana.jevtic@qcerris.com>
2024-03-17 02:16:45 +0100

    Fixed data processing function

---
+++ scripts/data_processing.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def process_actor_data(data, actor_column, actor_facebook_likes_column):
+    actor_count = data[actor_column].value_counts()
+    actor_df = pd.DataFrame(actor_count).reset_index().rename(columns={actor_column: 'actor_name', 'count': 'movie_count'})
+    actor_df = pd.merge(actor_df, data[[actor_column, actor_facebook_likes_column]], 
+                        how='left', 
+                        left_on='actor_name', 
+                        right_on=actor_column)
+    actor_df.drop(actor_column, axis=1, inplace=True)
+    actor_df.rename(columns={actor_facebook_likes_column: 'actor_facebook_likes'}, inplace=True)
+    actor_df.drop_duplicates(subset='actor_name', inplace=True)
+    actor_df = actor_df[actor_df['actor_name'] != 'unknown']
+    actor_df.reset_index(drop=True, inplace=True)
+    return actor_df
